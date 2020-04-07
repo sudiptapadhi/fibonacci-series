a49e11a2f7bc72ea1f9f56fa09c8f223f5d9e106
sudiptapadhi <63278866+sudiptapadhi@users.noreply.github.com>
2020-04-07 14:15:15 +0530

    datastr.py

---
+++ datastr.py
@@ -0,0 +1,18 @@
+ASSIGNING ELEMENTS TO DIFFERENT LIST
+test_list = [9, 3, 6, 5]
+print("The original list : " + str(list)) 
+list.extend(range(5)) 
+print("The list after adding range elements : " + str(test_list))
+
+ACCESSING DATA USING TUPLE
+tup1 = ('red', 'green', 9, 10);
+tup2 = (1, 2, 3, 4, 5, 6, 7 );
+print "tup1[0]: ", tup1[0];
+print "tup2[1:5]: ", tup2[1:5];
+
+DELETING DICTIONARY ELEMENT
+dict = {"zayn" : 22, "harry" : 21, "jazz" : 21, "arya" : 21}  
+print ("The dictionary before performing remove is : " + str(dict)) 
+del test_dict['jazz'] 
+print ("The dictionary after remove is : " + str(test_dict)) 
+  
